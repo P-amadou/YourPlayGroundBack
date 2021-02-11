a0fde1e6747e3fec6aaa275ec56e75575dfe51a5
Khazise <71380558+Khazise@users.noreply.github.com>
2021-02-11 12:12:21 +0100

    Update index.js

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,17 @@
+const {development}= require('../knexfile')
+const knex = require('../knex/knex.js')(development);
 const { Router } = require('express')
-
 const api = Router()
 
 api.get('/', (_, res) => {
     res.send('Hello API!')
   })
+
+api.get('/search',(req,res)=>{
+  let {nom}=req.query
+  knex("terrain").select("*").where("nom","like", nom).then(data => {
+    res.json(user.data)
+  })
+})
 
 module.exports = api
